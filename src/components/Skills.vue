69d7a9a7b39f5641c266e35648a483a239f6b774
Mahir Smajilbasic <mahir.smajilbasic@gmail.com>
2018-07-21 13:20:38 +0200

    Some animations

---
+++ src/components/Skills.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="hello">
+   <div class="holder">
+     <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a sill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+        
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
+
+     </form>     
+     <ul>
+       <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+       <li v-for="(data, index) in skills" :key="index">{{ data.skill }}</li>
+       </transition-group>
+     </ul>
+
+    <p>These are the skill that you possess.</p>
+
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skill: '',
+      skills: [
+        { "skill": "VueJS"},
+        { "skill": "PHP"},
+        { "skill": "Java"}
+      ],
+    }
+  },
+  methods: {
+    addSkill(){
+      this.$validator.validateAll().then((result) => {
+        if(result){
+            this.skills.push({skill: this.skill})
+            this.skill = '';
+        } else {
+          console.log('Not Valid');
+        }
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css";
+   .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-enter-leave {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+</style>
